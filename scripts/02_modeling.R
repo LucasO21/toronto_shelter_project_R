be83761d5f80cdbfa5f7a212f9e0fa873bf66f44
Lucas Okwudishu <62886078+LucasO21@users.noreply.github.com>
2023-10-11 05:22:00 -0400

    h2o test: save functions

--- scripts/02_modeling.R
+++ scripts/02_modeling.R
@@ -42,28 +42,116 @@ dbListTables(con)
 # DATA IMPORT ----
 # *****************************************************************************
 
-# * 2023 Shelter Data ----
-daily_shelter_tbl <- dplyr::tbl(con, "feature_shelter_2023") %>% 
+# * Import Data From BQ ----
+get_features_data_from_bigquery <- function(year = 2023) {
+  
+  con <- get_bigquery_connection(dataset = "data_features")
+  
+  tables <- DBI::dbListTables(con)
+  
+  shelter_tbl <- dplyr::tbl(con, str_glue("feature_shelter_{year}")) %>% 
     collect() %>% 
     mutate(occupancy_date = lubridate::ymd(occupancy_date)) %>% 
     mutate(location_address = str_trim(location_address, side = c("both"))) %>% 
     mutate(location_city = str_trim(location_city, side = c("both")))
+  
+  weather_tbl <- dplyr::tbl(con, str_glue("feature_weather_{year}")) %>% 
+    collect() %>% 
+    mutate(date = lubridate::ymd(date))
+  
+  # message
+  message(
+    str_glue(
+      "
+      Shelter Data Info:
+        nrow: {nrow(shelter_tbl)}
+        ncol: {ncol(shelter_tbl)}
+        min date: {min(shelter_tbl$occupancy_date)}
+        max date: {max(shelter_tbl$occupancy_date)}
+      ====================================================
+      Weather Data Info:
+        nrow: {nrow(weather_tbl)}
+        ncol: {ncol(weather_tbl)}
+        min date: {min(weather_tbl$occupancy_date)}
+        max date: {max(weather_tbl$occupancy_date)}
+      "
+    )
+  )
+  
+  # return 
+  return(list(shelter_tbl, weather_tbl))
+  
+}
 
-daily_shelter_tbl %>% glimpse()
-
-min(daily_shelter_tbl$occupancy_date)
-max(daily_shelter_tbl$occupancy_date)
+features_raw_list <- get_features_data_from_bigquery(year = 2023)
 
 
-# * 2023 Weather Data ----
-daily_weather_tbl <- dplyr::tbl(con, "feature_weather_2023") %>% 
-  collect() %>% 
-  mutate(date = lubridate::ymd(date))
+# * Prep Weather Data ----
+get_weather_data_prepped <- function(x) {
+  
+  # diff dates
+  shelter_dates <- unique(x[[1]]$occupancy_date)
+  weather_dates <- unique(x[[2]]$date)
+  diff_dates    <- shelter_dates[!shelter_dates %in% weather_dates]
+  
+  # tibble of diff dates
+  weather_avg <- x[[2]] %>% 
+    arrange(date) %>% 
+    tail(6) %>% 
+    bind_rows(
+      tibble(
+        stn  = "712650",
+        date = c(diff_dates)
+      )
+    ) 
+  
+  # get count na values
+  na_length <- weather_avg %>% filter(is.na(temp_min)) %>% pull(temp_min) %>% length() + 1
+  
+ # rolling avg for na values
+ weather_avg <- weather_avg %>%  
+    mutate(
+      across(
+        starts_with("temp_"),
+        list(
+          filled = ~ifelse(
+            is.na(.),
+            slide_dbl(., ~mean(.x, na.rm = TRUE), .before = na_length  - 1, .complete = TRUE),
+            .
+          )
+        ),
+        .names = "{col}_{fn}"
+      )
+    ) %>% 
+    select(-c(temp_min, temp_max, temp_avg)) %>% 
+    setNames(names(.) %>% str_remove_all("_filled")) %>% 
+    filter(date %in% diff_dates)
+ 
+ # append rolling avg weather
+ weather_tbl <- x[[2]] %>% bind_rows(weather_avg)
+    
+  
+  # return
+  return(weather_tbl)
+  
+}
 
-daily_weather_tbl %>% glimpse()
+get_weather_data_prepped(x = features_raw_list)
 
-min(daily_weather_tbl$date)
-max(daily_weather_tbl$date)
+
+# * Combine Data ----
+daily_combined_tbl <- features_raw_list[[1]] %>% 
+  #select(occupancy_date) %>% 
+  left_join(
+    get_weather_data_prepped(features_raw_list) %>% select(-stn),
+    by = c("occupancy_date" = "date")
+  )
+
+daily_combined_tbl %>% sapply(function(x) sum(is.na(x)))
+
+daily_combined_tbl %>% filter(is.na(days_open)) %>% View()
+
+
 
 
 # *****************************************************************************
@@ -73,7 +161,7 @@ max(daily_weather_tbl$date)
 
 # * Check For Nulls ----
 daily_shelter_tbl %>% 
-    sapply(function(x) sum(is.na(x)))
+  sapply(function(x) sum(is.na(x)))
 
 # * Distinct Counts ----
 
@@ -95,23 +183,23 @@ daily_shelter_tbl %>% distinct(overnight_service_type)
 
 # * Actual Occupancy by Type ----
 daily_shelter_tbl %>% 
-    count(program_model, sort = TRUE) %>% 
-    mutate(pct = n/sum(n))
+  count(program_model, sort = TRUE) %>% 
+  mutate(pct = n/sum(n))
 
 
 # * Actual Occupancy Shelter ----
 daily_shelter_tbl %>% 
-    count(shelter_id, sort = TRUE) %>% 
-    mutate(pct = n/sum(n)) %>% 
-    mutate(pct_cum = cumsum(pct)) %>% 
-    head(10)
+  count(shelter_id, sort = TRUE) %>% 
+  mutate(pct = n/sum(n)) %>% 
+  mutate(pct_cum = cumsum(pct)) %>% 
+  head(10)
 
 daily_shelter_tbl %>% 
-    filter(shelter_id == 24) %>% View()
+  filter(shelter_id == 24) %>% View()
 
 daily_shelter_tbl %>% 
-    select(capacity_type, program_area) %>% 
-    distinct()
+  select(capacity_type, program_area) %>% 
+  distinct()
 
 
 # *****************************************************************************
@@ -121,11 +209,11 @@ daily_shelter_tbl %>%
 
 # * Top Orgs by Location ----
 daily_shelter_tbl %>% 
-    summarise(
-        n   = n_distinct(location_id),
-        .by = c(organization_name, organization_id)
-    ) %>% 
-    arrange(desc(n))
+  summarise(
+    n   = n_distinct(location_id),
+    .by = c(organization_name, organization_id)
+  ) %>% 
+  arrange(desc(n))
 
 
 # *****************************************************************************
@@ -134,18 +222,18 @@ daily_shelter_tbl %>%
 # *****************************************************************************
 
 #' test modeling with 3 organizations (1, 15 and 6)
-    
+
 # * Pick Top 3 Locations ----
 daily_shelter_tbl %>% 
-    filter(organization_id %in% c(1)) %>% 
-    #filter(modeling_cohort == 1) %>% 
-    distinct(organization_name, shelter_group, location_name, location_address, location_city) %>% 
-    View()
-    
+  filter(organization_id %in% c(1)) %>% 
+  #filter(modeling_cohort == 1) %>% 
+  distinct(organization_name, shelter_group, location_name, location_address, location_city) %>% 
+  View()
+
 # * Filter for Org 1 ----
 sample_shelter_tbl <- daily_shelter_tbl %>% 
-    filter(organization_id %in% c(1, 15, 6)) %>% 
-    filter(modeling_cohort == 1)
+  filter(organization_id %in% c(1, 15, 6)) %>% 
+  filter(modeling_cohort == 1)
 
 sample_shelter_tbl %>% distinct(location_address) %>% nrow()
 
@@ -159,21 +247,27 @@ sample_shelter_tbl %>% glimpse()
 # *****************************************************************************
 
 # * Modeling Data ----
-modeling_tbl <- sample_shelter_tbl %>% 
-  select(
-    x_id, occupancy_date, organization_id, shelter_id, location_id, program_id,
-    sector, program_model, overnight_service_type, program_area, capacity_type, 
-    occupied, capacity_actual, occupancy_rate
-  ) %>% 
-  mutate(occupancy_rate = ifelse(occupancy_rate == 100, "Yes", "No") %>% as.factor()) %>% 
-  mutate_if(is_character, as_factor) %>% 
-  mutate(organization_id = as.factor(organization_id)) %>% 
-  mutate(shelter_id = as_factor(shelter_id)) %>% 
-  mutate(location_id = as_factor(location_id)) %>% 
-  mutate(program_id = as_factor(program_id)) 
+get_modeling_features <- function(data) {
+  
+  ret <- data %>% 
+    select(
+      x_id, occupancy_date, organization_id, shelter_id, location_id, program_id,
+      sector, program_model, overnight_service_type, program_area, capacity_type, 
+      occupied, capacity_actual, occupancy_rate
+    ) %>% 
+    mutate(occupancy_rate = ifelse(occupancy_rate == 100, "Yes", "No") %>% as.factor()) %>% 
+    mutate_if(is_character, as_factor) %>% 
+    mutate(organization_id = as.factor(organization_id)) %>% 
+    mutate(shelter_id = as_factor(shelter_id)) %>% 
+    mutate(location_id = as_factor(location_id)) %>% 
+    mutate(program_id = as_factor(program_id)) 
+  
+  return(ret)
+  
+}
 
-modeling_tbl %>% glimpse()
 
+modeling_tbl <- get_modeling_features(sample_shelter_tbl) %>% glimpse()
 
 # *****************************************************************************
 # **** ----
@@ -221,7 +315,7 @@ get_automl_recipes <- function(data, prob = TRUE) {
   }
   
   # recipe_spec <- recipe_spec %>% prep()
-    
+  
   return(recipe_spec)
   
   
@@ -232,7 +326,7 @@ get_automl_recipes(train_raw_tbl, TRUE) %>% juice() %>% glimpse()
 recipe_spec_prob <- get_automl_recipes(train_raw_tbl)
 
 recipe_spec_reg <- get_automl_recipes(train_raw_tbl, prob = FALSE)
-    
+
 
 # *****************************************************************************
 # **** ----
@@ -275,7 +369,7 @@ get_automl_models <- function(recipe, train_data, test_data, mrspm = 300,
     exclude_algos              = c("DeepLearning"),
     seed                       = seed
   )
-
+  
   return(
     list(
       h2o_automl_models = automl_models_h2o,
@@ -346,21 +440,24 @@ pred_reg_tbl <- automl_output_list_reg$h2o_automl_models@leader %>%
 pred_reg_tbl %>% View()
 
 
-
-
 # * Performance ----
 
-# ** Performance Object ----
-performance_h2o <- h2o.performance(stacked_ensemble_h2o, newdata = test_tbl_h2o)
 
-# ** Key Metrics ----
-h2o.auc(performance_h2o)
+# *****************************************************************************
+# **** ----
+# MLFLOW ----
+# *****************************************************************************
 
-h2o.confusionMatrix(performance_h2o) %>% as_tibble()
+# *****************************************************************************
+# **** ----
+# SAVE FUNCTIONS ----
+# *****************************************************************************
 
-h2o.precision(performance_h2o)
-
-# ** H2o Metric ----
-performance_tbl <- h2o.metric(performance_h2o) %>% 
-  as_tibble()
+# * Save Functions ----
+dump(
+  list = c("get_features_data_from_bigquery", "get_weather_data_prepped",
+           "get_modeling_features", "get_automl_recipes", "get_automl_models"),
+  file = "../functions/modeling.R",
+  append = FALSE
+)
 
